440c4232b5cbbf2fdd1b174d58214a681c0a3db9
xfdown <w22649@139.com>
2023-09-01 16:31:41 +0800

    fix

--- src/core/base-client.ts
+++ src/core/base-client.ts
@@ -261,10 +261,10 @@ export class BaseClient extends Trapper {
   }
 
   async switchQQVer(ver: string = '') {
-    if (this.sig.sign_api_addr && !this.sig.sign_api_init) {
-      await this.setSignServer(this.sig.sign_api_addr);
+    if (this.config.sign_api_addr && !this.sig.sign_api_init) {
+      await this.setSignServer(this.config.sign_api_addr);
     }
-    if (!this.config.ver) return false;
+    if (this.config.ver) return false;
     const old_ver = this.config.ver;
     this.config.ver = !ver ? await this.getApiQQVer() : ver;
     if (old_ver != this.config.ver) {
